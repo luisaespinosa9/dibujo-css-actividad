f7f8ae81b8fff3169d30e2b36698c8cdd0a50f63
luisaespinosa9 <l.espinosap@uniandes.edu.co>
2026-03-02 16:49:44 +0000

    java

---
+++ script.js
@@ -0,0 +1,9 @@
+//console.log("hola");
+
+const varTitulo = document.getElementById("titulo");
+
+varTitulo.addEventListener("click", () => {
+    varTitulo.innerText = "¡Buenos dìas!" ; 
+    varTitulo.style.color = "#32a" ;
+    
+})
